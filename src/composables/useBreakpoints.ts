615ca9bc8722fc7056cfff74facd54e57f098def
woojoongjc <woojoongjc@gmail.com>
2026-02-05 16:59:32 +0900

    fix: 빌드수정

--- src/composables/useBreakpoints.ts
+++ src/composables/useBreakpoints.ts
@@ -1,16 +1,18 @@
 const REM = 16
+
 export const useBreakpoints = () => {
-    const { width } = useWindowSize()
+    // SSR 안전 처리
+    const width = process.client ? useWindowSize().width : computed(() => 0)
 
     const isMobile = computed(() => width.value <= 30 * REM)
 
-    const isMobilePlus = computed(() => width.value > 30.0625 * REM && width.value <= 48 * REM)
+    const isMobilePlus = computed(() => width.value > 30 * REM && width.value <= 48 * REM)
 
-    const isTablet = computed(() => width.value > 48.0625 * REM && width.value <= 62 * REM)
+    const isTablet = computed(() => width.value > 48 * REM && width.value <= 62 * REM)
 
-    const isLaptop = computed(() => width.value > 62.0625 * REM && width.value <= 75 * REM)
+    const isLaptop = computed(() => width.value > 62 * REM && width.value <= 75 * REM)
 
-    const isPc = computed(() => width.value > 75.0625 * REM)
+    const isPc = computed(() => width.value > 75 * REM)
 
     return {
         isMobile,
